957e63334a9b4037b4e5eff25aa62e6d6ae7cb27
anandg112 <anandg112@gmail.com>
2017-05-02 00:16:30 -0400

    added circles and ellipses

--- shapes.js
+++ shapes.js
@@ -7,8 +7,36 @@ var svg = d3.select("body").append("svg")
 svg.selectAll("rect")
     .data(dataArr)
     .enter().append("rect")
-    .attr("height", function(d,i){return d*15})
+    .attr("height", function(d,i){return d * 15})
     .attr("width","50")
-    .attr("x", function(d,i){ return 60*i})
+    .attr("x", function(d,i){ return 60 * i})
     .attr("y", function(d,i){return 300-(d * 15)})
     .attr("fill", "pink");
+
+var newX = 300;
+svg.selectAll("circle.first")
+   .data(dataArr)
+   .enter().append("circle")
+   .attr("class", "first")
+   .attr("cx", function(d, i){ newX += (d * 3) + (i * 20); return newX;})
+   .attr("cy", "100")
+   .attr("r", function(d){return d * 3; });
+
+var newX = 600;
+svg.selectAll("ellipse")
+  .data(dataArr)
+  .enter().append("ellipse")
+  .attr("class", "second")
+  .attr("cx", function(d, i){ newX += (d * 3) + (i * 20); return newX;})
+  .attr("cy", "100")
+  .attr("rx", function(d){return d * 3; })
+  .attr("ry", "30")
+
+var newX = 900;
+svg.selectAll("line")
+  .data(dataArr)
+  .enter().append("line")
+  .attr("x1", newX)
+  .attr("y1", function(d, i){return 80 + (i * 20); })
+  .attr("x2", function(d){return newX + (d * 15); })
+  .attr("y2", "30")
